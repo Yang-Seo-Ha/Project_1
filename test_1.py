9f3aed8414dd148b226c13dac7381222ee3a7c39
Kim-Junghyeong <cxop2002@naver.com>
2025-11-24 14:38:36 +0900

    푸쉬위한커밋

---
+++ test_1.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.family'] = 'Malgun Gothic'
+plt.rcParams['axes.unicode_minus'] = False
+
+df = pd.read_csv("1회권 외국인 일별통행통계")
